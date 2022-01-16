38946745bf9fd6b0fe16b5236872fb21920ec600
Andrew Gorelov <lordwert@gmail.com>
2022-01-16 20:57:12 +0300

    6

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ def fun60():
     """
 
     """
-    print()
+    str = 'Я волна'
+    stren = str.encode("cp1251")
+    print(stren)
+    stren = stren.decode("cp1251")
+    print(stren)
+
+fun60()
