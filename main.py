ecf393757c317ff8f4ead6dd371d79142414590f
Andrew Gorelov <lordwert@gmail.com>
2022-01-16 23:27:51 +0300

    6

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 def fun60():
     """
 
@@ -5,7 +7,13 @@ def fun60():
     str = 'Я волна'
     stren = str.encode("cp1251")
     print(stren)
-    stren = stren.decode("cp1251")
+    strb = stren
+    for i in strb:
+        if i != 223:
+            print(i, ascii(i))
+    stren = stren.decode("utf-8", "replace")
     print(stren)
+    print(sys.getdefaultencoding())
+    print(strb)
 
 fun60()
