34c0c84c396cb02f637b6916208aa2a0615de201
Andrew <lordwert@gmail.com>
2022-01-17 16:34:39 +0300

    4 commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import sys
 
 def fun60():
@@ -14,6 +15,31 @@ def fun60():
     stren = stren.decode("utf-8", "replace")
     print(stren)
     print(sys.getdefaultencoding())
-    print(strb)
+    (strb)
+    # t = (6,5,9,0,6,0)
+    # print(t.count(0))
+    # print(type(t))
 
-fun60()
+from random import randint
+def fun601():
+    """
+    work with files
+    """
+    str2 = ''
+    with open("./log.txt", mode="r+") as hand:
+        str3 = hand.readlines()
+        #hand.flush()
+    #str = str[0:15]
+    for i in range(len(str3)):
+        if str3[i] !='x':
+            str2 += str3[i]
+            #print(str[i])
+            #print(f'Empty: {str[i]}')
+    #print(str[1].encode())
+    i = randint(56, 678)
+    print(f'I={i}')
+    with open("./log.txt", mode="a") as hand:
+        hand.writelines("New string " + str(i) + '\n')
+    print(str3)
+    print(len(str3))
+fun601()
