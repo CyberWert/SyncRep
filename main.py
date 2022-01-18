950cc04e309aa91e1daeb28de51a46eb1d2eea72
Andrew Gorelov <lordwert@gmail.com>
2022-01-18 22:14:57 +0300

    7lesson

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ import csv
 
 def fun70():
     with open("./files/7lesson/CSVfile.csv", mode='w') as fl:
-        wr = csv.writer(fl)
-        wr.writerow(['456','677', '554'])
-        wr.writerow([5,6,7])
+        wr = csv.DictWriter(fl, fieldnames=['First', 'Second', 'ffdf'], restval=100)
+        wr.writerow({'First': 45, 'Second': 567})
+        wr.writeheader()
+        #wr.writerow(['456','677', '554'])
+        #wr.writerow([5] * 3)
     with open("./files/7lesson/CSVfile.csv", mode='r') as fl:
-        rr = csv.reader(fl)
+        #rr = csv.reader(fl)
+        rr = csv.DictReader(fl, fieldnames=['First', 'Second'], restkey='Reserved', restval='Empty')
         for i in rr:
             print(i)
 
-fun70()
+import json
+def fun701():
+    """
+    json
+    """
+    d = {'name': 'ffdf', 'phones': [6786567, 5671234]}
+    print(d)
+    json.dumps(d)
+
+fun701()
