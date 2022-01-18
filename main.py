3af409d08cac01bf40e846eec115b84214b7e6db
Andrew <lordwert@gmail.com>
2022-01-18 17:27:05 +0300

    7 lesson

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import csv
 
 def fun70():
-    with open("./files/7lesson/CSV.csv", mode='r') as fl:
+    with open("./files/7lesson/CSVfile.csv", mode='w') as fl:
         wr = csv.writer(fl)
-        wr.writerow(['456','677',554])
-    print()
+        wr.writerow(['456','677', '554'])
+        wr.writerow([5,6,7])
+    with open("./files/7lesson/CSVfile.csv", mode='r') as fl:
+        rr = csv.reader(fl)
+        for i in rr:
+            print(i)
+
+fun70()
