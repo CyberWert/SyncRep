fdea817c1cd3d8fa99b296ee57ae3d909eeba67d
Andrew <lordwert@gmail.com>
2022-01-19 16:44:44 +0300

    7 lesson 2 task

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+
+class ABC(object):
+    def __init__(self, a):
+        self.a = a
+    def cl_get(self):
+        print(self.a)
+    def cl_set(self, num):
+        self.a = num
+
 def fun80():
-    print()
+    cl = ABC(67)
+    cl.cl_set(56)
+    cl.cl_get()
+    #root.pri(8)
+
+fun80()
