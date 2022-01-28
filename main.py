0fa2dd91b00635c14356808d4f6ffc33c0483f39
Andrew <lordwert@gmail.com>
2022-01-28 19:13:33 +0300

    10 lesson

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 def tasks():
-    pass
+    ls = [x ** y for x in range(2, 5) for y in range(9)]
+    print(ls)
+    ls2 = [[2, 3, 6], [5, 6, 7], [3, 8, 0]]
+    ls3 = [j for i in ls2 for j in i]
+    di2 = {'Key2': 45, 'DSD': 234}
+    di3 = [di2[k] for k in di2]
+    ls4 = [[x + y for x in 'ABCD'] for y in '1234']
+    ls5 = [j for i in ls4 for j in i]
+    ls6 = [[str(x) + str(y) for x in range(10) if x - y > 0] for y in range(10)]
+
+    print(ls6)
+
+
+tasks()
